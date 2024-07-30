86c598c91abb55d632efcb0550dfdb2b58d51882
Marília Fernandez <marilia.fernandez@accenture.com>
2024-07-30 14:51:10 -0300

    organize project structure, persist vectordb

--- src/Chat.py
+++ src/Chat.py
@@ -1,8 +1,11 @@
 from typing import List, Dict
-from Vectordb import Vectordb
+from src.Vectordb import Vectordb
+from transformers import pipeline
 
 BASE_PROMPT = """You are a helpfull and respectfull assistant who is responsible for answering user questions about BLOOD DONATION. 
-Please keep your answers concise and objective. 
+Keep your answers strictly to the subject of BLOOD DONATION and refuse poletely to answer to any other subject.
+Please keep your answers concise and objective.
+Use the given examples to generate your final answer.
 If you don't know the answer, DO NOT create one, but suggest the user to get the information in official websites."""
 
 
@@ -10,11 +13,17 @@ class Chat:
     def __init__(self) -> None:
         self.vectordb = Vectordb()
         self.collection = self.vectordb.get_collection("faq_redcross")
+        self.pipe = pipeline(
+            "text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+        )
 
     def ask(self, question: str) -> str:
-        # TODO: add LLM to generate answer
         samples = self._get_related_samples(question)
-        prompt = self.build_prompt(samples)
+        prompt = self._build_prompt(samples)
+        result = self.pipe(
+            [{"role": "system", "content": prompt}, {"role": "user", "content": question}]
+        )
+        return result[0]['generated_text'][-1]['content']
 
     def _get_related_samples(self, question: str) -> List[Dict]:
         return self.vectordb.retrieve(question, self.collection)
--- src/Vectordb.py
+++ src/Vectordb.py
@@ -1,32 +1,34 @@
-from chromadb import Client, Collection
+from chromadb import Collection, PersistentClient
 from typing import Dict, List
 import json
+from uuid import uuid4
 
 
 class Vectordb:
     def __init__(self) -> None:
-        self.client = Client()
+        self.client = PersistentClient(path="./chromadb")
 
     def create_collection(self, collection_name: str) -> Collection:
         with open("data/faq.json", "r", encoding="utf-8") as fp:
             data = json.load(fp)
         collection = self.client.create_collection(name=collection_name)
-        self.add_faq_records(collection, data["faqs"])
+        self.add_faq_records(data["faqs"], collection)
         return collection
 
-    def get_collection(self, collection_name: str) -> Collection:
+    def get_collection(self, collection_name) -> Collection:
         try:
             return self.client.get_collection(name=collection_name)
         except Exception:
             return self.create_collection(collection_name)
 
-    def add_faq_records(self, collection: Collection, faq_records: List[Dict]) -> None:
-        documents = [
-            f"Category: {item['category']} \n\nQ: {item['title']} \n\nA: {item['description']}"
-            for item in faq_records
-        ]
+    def add_faq_records(
+        self,
+        faq_records: List[Dict],
+        collection: Collection,
+    ) -> None:
+        documents = [self._build_faq_string(item) for item in faq_records]
         collection.add(
-            documents=documents, ids=[f"id_{i}" for i in range(len(documents))]
+            documents=documents, ids=[str(uuid4()) for _ in range(len(documents))]
         )
         print(
             f"Added {len(documents)} records, collection {collection.name} has now {collection.count()} records."
@@ -37,3 +39,8 @@ class Vectordb:
             query_texts=[query], n_results=n, include=["documents"]
         )
         return results["documents"][0]
+
+    def _build_faq_string(self, faq_record: Dict) -> str:
+        if faq_record["category"] != None and (faq_record["category"]).strip() != "":
+            return f"Category: {faq_record['category']} \n\nQ: {faq_record['title']} \n\nA: {faq_record['description']}"
+        return f"Q: {faq_record['title']} \n\nA: {faq_record['description']}"
